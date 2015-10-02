91e3728fb8c1b986df411b0ed0b32dddb2243d21
Phil Pursglove <phil@philippursglove.com>
2015-10-02 13:46:39 +0100

    Initial checkin

---
+++ RavenBooks/Controllers/BookController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RavenBooks.Models;
+
+namespace RavenBooks.Controllers
+{
+    public class BookController : RavenController
+    {
+        //
+        // GET: /Book/
+
+        public ActionResult Index()
+        {
+            return View(RavenSession.Query<Book>().ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                RavenSession.Store(book);
+                RavenSession.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(book);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Book book = RavenSession.Load<Book>(id);
+
+            return View(book);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                RavenSession.Store(book);
+                RavenSession.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(book);
+        }
+
+        public ActionResult Show(int id)
+        {
+            return View(RavenSession.Load<Book>(id));
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Book book = RavenSession.Load<Book>(id);
+
+            return View(book);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Book book)
+        {
+            int id = book.Id;
+            Book bookToDelete = RavenSession.Load<Book>(id);
+            RavenSession.Delete(bookToDelete);
+            RavenSession.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ RavenBooks/Controllers/RavenController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Raven.Client;
+
+namespace RavenBooks.Controllers
+{
+    public class RavenController : Controller
+    {
+        public IDocumentSession RavenSession;
+
+         protected override void OnActionExecuting(ActionExecutingContext filterContext) 
+        { 
+            RavenSession = MvcApplication.bookDocumentStore.OpenSession(); 
+        } 
+ 
+        protected override void OnActionExecuted(ActionExecutedContext filterContext) 
+        { 
+            if (filterContext.IsChildAction) 
+                return; 
+ 
+            using (RavenSession) 
+            { 
+                if (filterContext.Exception == null) 
+                    RavenSession.SaveChanges(); 
+            } 
+        } 
+    }
+}
